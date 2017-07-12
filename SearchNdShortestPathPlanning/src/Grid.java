41bd9412912be0ea0d43d7a4c56fdb22679b81ce
murtalaliyu <murtalaliyu10@yahoo.com>
2017-07-12 10:42:21 -0400

    fixed serial numbers

--- SearchNdShortestPathPlanning/src/Grid.java
+++ SearchNdShortestPathPlanning/src/Grid.java
@@ -2,13 +2,14 @@
 public class Grid {
 
 	public static void makeGrid(Node grid, int cellNumber) {
-		
+		int sum = 0;
 		for (int row = 0; row < cellNumber; row++) {
 			for (int col = 0; col < cellNumber; col++) {
-				grid.serialNo = row+col;
+				grid.serialNo = row+col+sum;
 				grid.status = "0";
 				System.out.print(grid.serialNo + "   ");
 			}
+			sum += cellNumber-1;
 			System.out.println();
 		}
 		
