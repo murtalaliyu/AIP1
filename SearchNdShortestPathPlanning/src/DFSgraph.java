4ee8e2e83e5b53ed83c48845c165a58e03370570
murtalaliyu <murtalaliyu10@yahoo.com>
2017-07-12 22:42:56 -0400

    dfs changes

--- SearchNdShortestPathPlanning/src/DFSgraph.java
+++ SearchNdShortestPathPlanning/src/DFSgraph.java
@@ -1,11 +1,3 @@
-/*change String[][] to node with blocked/unblocked, serial number (to determine neighbor)
-0	1	2	3	4
-5	6	7	8	9
-10	11	12	13	14		<--- 5x5 grid
-15	16	17	18	19	
-20	21	22	23	24
-*/
-
 import java.util.ArrayList;
 import java.util.Stack;
 
