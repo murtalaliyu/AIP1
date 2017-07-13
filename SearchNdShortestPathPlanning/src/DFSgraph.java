cfbc2a774f072e1bfa1445d2aff090351413f277
murtalaliyu <murtalaliyu10@yahoo.com>
2017-07-12 23:46:47 -0400

    dfs not working properly

--- SearchNdShortestPathPlanning/src/DFSgraph.java
+++ SearchNdShortestPathPlanning/src/DFSgraph.java
@@ -7,53 +7,66 @@ public class DFSgraph {
 	static ArrayList visited = new ArrayList<Node>();
 
 	public static Node[][] DFSsearch(Node[][] grid, Node node) {
-	
 		
-		if (node.status == "0" && !visited.contains(node)) {
+		do {
 			visited.add(node);
 			grid[node.first][node.second].status = "1";
 			
 			//get this node's neighbors and add to stack
 			int x = node.first;
 			int y = node.second;
-			Node newNeighbor = null;
+			Node newNeighbor = new Node(0,0,"0");
+			Object object = new Object();
 			//top
-			if (x-1 >= 0) {
-				if (grid[x-1][y].status == "0") {
-					newNeighbor.first = x-1;
-					newNeighbor.second = y;
-					newNeighbor.status = grid[x-1][y].status;
-					stack.push(newNeighbor);
+			object = x-1;
+			if (object instanceof Integer == true) {
+				if ((int)object != -1) {
+					if (grid[x-1][y].status == "0") {
+						newNeighbor.first = x-1;
+						newNeighbor.second = y;
+						newNeighbor.status = grid[x-1][y].status;
+						stack.push(newNeighbor);
+					}
 				}
 			} 
-			if (x+1 < grid.length) {
-				if (grid[x+1][y].status == "0") {
-					newNeighbor.first = x+1;
-					newNeighbor.second = y;
-					newNeighbor.status = grid[x+1][y].status;
-					stack.push(newNeighbor);
+			object = x+1;
+			if (object instanceof Integer == true) {
+				if ((int)object != grid.length) {
+					if (grid[x+1][y].status == "0") {
+						newNeighbor.first = x+1;
+						newNeighbor.second = y;
+						newNeighbor.status = grid[x+1][y].status;
+						stack.push(newNeighbor);
+					}
 				}
 			}
-			if (y-1 >= 0) { 
-				if (grid[x][y-1].status == "0") {
-					newNeighbor.first = x;
-					newNeighbor.second = y-1;
-					newNeighbor.status = grid[x][y-1].status;
-					stack.push(newNeighbor);
+			object = y-1;
+			if (object instanceof Integer == true) {
+				if ((int)object != -1) { 
+					if (grid[x][y-1].status == "0") {
+						newNeighbor.first = x;
+						newNeighbor.second = y-1;
+						newNeighbor.status = grid[x][y-1].status;
+						stack.push(newNeighbor);
+					}
 				}
 			}
-			if (y+1 < grid.length) {
-				if (grid[x][y+1].status == "0") {
-					newNeighbor.first = x;
-					newNeighbor.second = y+1;
-					newNeighbor.status = grid[x][y+1].status;
-					stack.push(newNeighbor);
+			object = y+1;
+			if (object instanceof Integer == true) {
+				if ((int)object != grid.length) {
+					if (grid[x][y+1].status == "0") {
+						newNeighbor.first = x;
+						newNeighbor.second = y+1;
+						newNeighbor.status = grid[x][y+1].status;
+						stack.push(newNeighbor);
+					}
 				}
 			}
 			//pop stack and call dfs on node
-			grid = DFSsearch(grid,(Node)stack.pop());
-			System.out.println(stack);
-		}
+			if (stack.empty() != true) {
+				grid = DFSsearch(grid,(Node)stack.pop());
+			}
+		} while (!stack.empty());
 		
 		return grid;
 	}
--- SearchNdShortestPathPlanning/src/Grid.java
+++ SearchNdShortestPathPlanning/src/Grid.java
@@ -4,21 +4,6 @@ public class Grid {
 	public static Node[][] makeGrid(Node[][] grid) {
 		Node node = new Node(0,0,"0");
 		int sum = 0;
-		for (int i = 0; i < grid.length; i++) {
-			for (int j = 0; j < grid.length; j++) {
-				node = new Node(0,0,"0");
-				node.first = i;
-				node.second = j;
-				node.status = "0";
-				grid[i][j] = node;
-				//System.out.print(node.first + "," + node.second + "   ");
-				System.out.print(node.first + "," + node.second + "   ");
-			}
-			sum += grid.length-1;
-			System.out.println();
-		}
-		
-		System.out.println();
 		
 		for (int i = 0; i < grid.length; i++) {
 			for (int j = 0; j < grid.length; j++) {
